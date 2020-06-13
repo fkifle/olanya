fccb43af77647da64747761d1205c253d4cfd6b0
Frezer Kifle <frezer.kifle@mantech.com>
2020-06-12 21:10:20 -0400

    more stuff to get it to work to push to heroku

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+const $PORT = process.env.PORT || 3000;
 app.listen($PORT, function () {
   console.log('Example app listening on port 3000!');
 });
